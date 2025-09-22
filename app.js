774a6cd9f1fd2ea4b8548378b13a5cd0bdc30849
Poojitha-git16 <kummaripoojitha10@gmail.com>
2025-09-22 11:02:08 +0530

    datatype

--- app.js
+++ app.js
@@ -2,6 +2,15 @@ var a=67;
 var a=90;
 let b=68;
 const c=5;
-console.log(`value c is${c}`);
+console.log(`value c is ${c}`);
 console.log(`value b is ${b}`);
+console.log(a);
+//commit used to understand the code ny other developer
+//data types
+//1.primitive 2. Non primitive
+//1-Number,String Boolean,Null,Undefined
+//
+// typeof()-know the type of data
+//this ,window-commands to display the functions of js
+a=98;
 console.log(a);
---
+++ datatypes.js
@@ -0,0 +1,19 @@
+a=28;
+console.log(typeof a);
+name ="poojitha"; 
+console.log(typeof name);
+c=45;
+console.log(typeof a);
+b=true;
+console.log(typeof b);
+data =null;
+console.log(typeof data);
+array=[34,"john",true,null];
+console.log(typeof array);
+//onject declaration
+data={
+    name:"pooji",
+    age:"78",
+    dept:"CSE",
+};
+console.log(typeof data);
