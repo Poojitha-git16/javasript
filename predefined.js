c8e72d1655ea0441b017889318f922a776044cc0
Poojitha-git16 <kummaripoojitha10@gmail.com>
2025-09-22 16:11:54 +0530

    predefined

---
+++ predefined.js
@@ -0,0 +1,24 @@
+
+var a=Number(prompt("Enter value a "));
+var b=Number(prompt("Enter value b "));
+// document.write(alert(a+b));
+// document.write(alert(a*b));
+// document.write(alert(a-b));
+document.write(`Entered values and b is ${a},${b}`);
+function add()
+{
+    alert(a+b);
+}
+function sub()
+{
+    alert(a-b);
+}
+function mult()
+{
+    alert(a*b);
+
+}
+function div()
+{
+    alert(a/b);
+}
